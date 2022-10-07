3f4d493d210c0b092e20f9c52c619047d12e417f
Боярников Семён <boysemyon@mail.ru>
2022-10-07 21:39:52 +0600

    chore: настройка проекта

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = (env, argv) => {
+  return {
+    entry: './src/index.ts',
+    devtool: argv.mode === 'production' ? 'source-map' : 'inline-source-map',
+    devServer: {
+      static: './dist'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: true
+              }
+            },
+            {
+              loader: 'stylus-loader'
+            }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Development'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].stories.css'
+      })
+    ],
+    output: {
+      filename: '[name].stories.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    }
+  };
+};
